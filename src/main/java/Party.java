57371dfe3ad91ee27661b1b5a6f6bcdcb47d0edf
Sheena Nickerson <sheenanick@gmail.com>
2016-09-09 08:54:26 -0700

    add second test to test calculateCost method - test failing

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -3,12 +3,44 @@ class Party {
   private String mFood;
   private String mBeverages;
   private String mEntertainment;
+  private int mFoodCost;
+  private int mBeverageCost;
+  private int mEntertainmentCost;
 
   public Party(int guests, String food, String beverages, String entertainment) {
     mNumOfGuests = guests;
     mFood = food;
     mBeverages = beverages;
     mEntertainment = entertainment;
+  }
+
+  public int calculateCost() {
+    if (mFood.equals("full course")) {
+      int mFoodCost = 75;
+    } else if (mFood.equals("light meal")) {
+      int mFoodCost = 15;
+    } else {
+      int mFoodCost = 5;
+    }
+
+    if (mBeverages.equals("full bar")) {
+      int mBeverageCost = 50;
+    } else if (mBeverages.equals("light refreshments")) {
+      int mBeverageCost = 5;
+    } else {
+      int mBeverageCost = 2;
+    }
+
+    if (mEntertainment.equals("live band")) {
+      int mEntertainmentCost = 2000;
+    } else if (mEntertainment.equals("DJ")) {
+      int mEntertainmentCost = 1000;
+    } else {
+      int mEntertainmentCost = 0;
+    }
+
+    int partyCost = ((mFoodCost + mBeverageCost) * mNumOfGuests + mEntertainmentCost);
+    return 0;
   }
 
 }
--- src/test/java/PartyTest.java
+++ src/test/java/PartyTest.java
@@ -9,4 +9,9 @@ public class PartyTest {
     assertEquals(true, testParty instanceof Party);
   }
 
+  @Test
+  public void newParty_calculatesCost_70() {
+    Party testParty = new Party(10, "snack", "water", "none");
+    assertEquals(70, testParty.calculateCost());
+  }
 }
