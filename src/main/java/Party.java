3258cf6153e131020f76788f7f43073ec54df3a2
Sheena Nickerson <sheenanick@gmail.com>
2016-09-09 08:22:59 -0700

    pass first test - Party object is instantiating correctly

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,4 +1,14 @@
 class Party {
+  private int mNumOfGuests;
+  private String mFood;
+  private String mBeverages;
+  private String mEntertainment;
 
-  
+  public Party(int guests, String food, String beverages, String entertainment) {
+    mNumOfGuests = guests;
+    mFood = food;
+    mBeverages = beverages;
+    mEntertainment = entertainment;
+  }
+
 }
--- src/test/java/PartyTest.java
+++ src/test/java/PartyTest.java
@@ -3,6 +3,10 @@ import static org.junit.Assert.*;
 
 public class PartyTest {
 
-
+  @Test
+  public void newParty_instantiatesCorrectly() {
+    Party testParty = new Party(10, "snack", "water", "none");
+    assertEquals(true, testParty instanceof Party);
+  }
 
 }
